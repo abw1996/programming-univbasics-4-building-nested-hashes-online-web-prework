59fe4945cbd8aa9de4f73dd1b39c119e61416e04
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-31 17:05:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,8 @@ monopoly = {railroads: {
     names: {
       reading_railroad: {},
       pennsylvania_railroad: {},
-      b_and_o_railroad: {}
+      b_and_o_railroad: {},
+      shortline: {}
     },
     rent_in_dollars: {
       one_piece_owned: 25,
@@ -40,7 +41,8 @@ monopoly = {railroads: {
     names: {
       reading_railroad: {},
       pennsylvania_railroad: {},
-      b_and_o_railroad: {}
+      b_and_o_railroad: {},
+      shortline: {}
     },
     rent_in_dollars: {
       one_piece_owned: 25,
