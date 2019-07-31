82113ac4b4f86532d392e681e4acfbea50082798
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-31 16:53:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,8 @@ monopoly = {railroads: {
     names: {},
     rent_in_dollars: {
       one_piece_owned: 25,
-      two_pieces_owned: 50
+      two_pieces_owned: 50,
+      three_pieces_owned: 100
     }
   }
 }
